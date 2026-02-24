496443f81284130a917a4e78132f1bdc9d175b29
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2026-02-24 21:30:45 +0545

    Almost backend over and Frontend setup

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,62 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js"
+export const getUsersForSideBar = async(req,res)=>{
+    try {
+        const loggedInUser=req.user._id;
+        const filterUsers = await User.find({_id: {$ne: loggedInUser}}).select("-password");
+
+        res.status(200).json({filterUsers});
+    } catch (error) {
+        console.log("Error in Getting Users for Sidebar",error.message);
+    res.status(500).json({error: "Internal Server Error"});
+    }
+};
+export const getMessages = async(req,res)=>{
+    try {
+        const {id:userToChatId} = req.params;
+        const myId = req.user._id;
+        
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId}
+            ]
+        })
+        res.status(200).json(messages);
+    }
+    catch (error) {
+        console.log("Error in getMessages controller",error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+};
+export const sendMessage = async(req,res)=>{
+    try {
+        const {text,image}= req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image)
+        {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        //todo realtime features go here 
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in sendMessage Controller",error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+};
