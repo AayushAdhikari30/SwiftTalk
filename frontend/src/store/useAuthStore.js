69425fd6f9c8cdaa81af8d5a8e8706b3d4381c63
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2026-02-25 21:35:34 +0545

    Signup page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,26 +1,25 @@
-import {create} from "zustand"
+import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 
-export const useAuthStore = create((set)=> ({
-    authUser:null,
-    isSigningUp:null,
-    isLoggingIn:null,
-    isUpdatingProfile:null,
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: null,
+  isLoggingIn: null,
+  isUpdatingProfile: null,
 
-    isCheckingAuth : true,
+  isCheckingAuth: true,
 
-    checkAuth:  async() =>
-        {
-            try {
-                const res = await axiosInstance.get("/auth/check");
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
 
-                set({authUser:res.data});
-            } catch (error) {
-                console.log("Not authorized",error.message)
-                set({authUser:null});
-            }finally
-            {
-                set({isCheckingAuth: false})
-            }
-        }
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Not authorized", error.message);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+  signup: async (data) => {},
 }));
