2bc80171a723425824b6bbcac2d15b2012e33063
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2026-03-03 23:23:31 +0545

    Url errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -15,12 +16,13 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+
+app.use(cors({
+  origin: process.env.NODE_ENV === "production" 
+    ? "https://swifttalk-6f7h.onrender.com" 
+    : "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,10 +7,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: process.env.NODE_ENV === "production"
+      ? ["https://swifttalk-6f7h.onrender.com"]
+      : ["http://localhost:5173"],
+    credentials: true,
   },
 });
-
 export function getRecieverSocketId (userId) {
   return userSocketMap[userId];
 }
